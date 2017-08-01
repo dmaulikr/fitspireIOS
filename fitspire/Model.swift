c7ccb08a5c5ba71ff0a1e80392c43286c2170f7a
lunaca <skoorbcaz@gmail.com>
2017-08-01 16:56:46 -0500

    Resurrection
    File Movement error wrecked project but i recovered it. Added Browse
    Exercises and implemented search Feature

--- fitspire/Model.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Model.swift
-//  fitspire
-//
-//  Created by LUNVCA on 7/22/17.
-//  Copyright © 2017 uca. All rights reserved.
-//
-
-import Foundation
--- fitspire/WorkoutItem.swift
+++
@@ -1,7 +0,0 @@
-//
-//  WorkoutItem.swift
-//  fitspire
-//
-//  Created by LUNVCA on 1/24/17.
-//  Copyright © 2017 uca. All rights reserved.
-//
